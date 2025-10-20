2f144f8255bd997e793fa4c510cc8310f259a9d3
mrvijaykumar07 <kumarbijaybehera07@gmail.com>
2025-10-20 20:08:11 +0530

    Updated padding and heights

--- src/Components/LandingPage.jsx
+++ src/Components/LandingPage.jsx
@@ -4,18 +4,19 @@ const Photosection = () => {
   return (
     <section
       id="home"
-      className="hero bg-gray-800  mx-auto flex w-full h-screen pb-20  md:px-44"
+      className="hero bg-gray-800  mx-auto flex w-full h-screen md:py-20  md:px-44"
     >
       <div className="flex flex-col  justify-start container ">
-        <div className=" w-screen h-full flex items-center justify-center ">
-          <img
-            src="/assets/images/mobileBanner.jpg"
-            alt="Bijaya Kumar"
-            className=" w-96 h-[240px] object-cover  md:hidden mb-3 "
-          />
-        </div>
+    <div className="w-full flex items-center justify-center md:hidden mt-4">
+  <img
+    src="/assets/images/mobileBanner.jpg"
+    alt="Bijaya Kumar"
+    className="w-96 h-[240px] object-cover"
+  />
+</div>
 
-        <div className="flex flex-col  justify-center container  px-6">
+
+        <div className="flex flex-col  justify-center container md:py-8 pt-8 px-6">
           <h1 className="md:text-2xl text-xl font-bold mb-3">
             Representing....
           </h1>
@@ -44,11 +45,11 @@ const Photosection = () => {
         </div>
       </div>
 
-      <div className="mt-8">
+      <div className="mt-6 hidden md:block ml-7">
         <img
           src="/assets/images/blinkiGif.gif"
           alt="Bijaya Kumar"
-          className="rounded-full w-[820px] h-[420px] object-cover  hidden md:block "
+          className="rounded-full w-[820px] h-[380px] object-cover  hidden md:block "
         />
       </div>
     </section>
